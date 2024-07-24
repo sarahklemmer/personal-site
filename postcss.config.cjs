da0d410b5dd84f2fc7b858cdfa613494e1999e68
srhklm <157084753+srhklm@users.noreply.github.com>
2024-07-24 14:15:08 -0700

    basic navbar

---
+++ postcss.config.cjs
@@ -0,0 +1,7 @@
+/** @type {import('postcss').PluginCreator} */
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li href="#home">Home</li>
+                <li href="#about">About</li>
+                <li href='#experience'>Experience</li>
+                <li href='#contact'>Contact Me</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
